49988618b780b2f7ea8fff2948de4d864b937ffe
JohnnyLau1028 <81736997+JohnnyLau1028@users.noreply.github.com>
2021-11-22 00:56:40 -0500

    motion

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,25 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { ExpoScaleEase, RoughEase, SlowMo } from "gsap/EasePack";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, ExpoScaleEase, RoughEase, SlowMo);
+
+const mainTL = gsap.timeline();
+
+function line1(){
+    const tl= gsap.timeline();
+
+    tl.from("#line1", {duration:1, drawSVG: "0%"})
+
+    return tl;
+}
+
+
+mainTL.add(line1());
+
+
+GSDevTools.create();
+
+
